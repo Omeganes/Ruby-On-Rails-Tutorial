53cbdfd927743715291326389f024cc34dfbfe52
Raymond Youssef <rimonomega@gmail.com>
2021-04-27 19:59:17 +0200

    A working form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,20 @@
 class UsersController < ApplicationController
 
-  def show
-    @user = User.find(params[:id])
-    # debugger
-  end
+    def show
+        @user = User.find(params[:id])
+        # debugger
+    end
 
+    def new
+        @user = User.new
+    end
 
-  def new
-    @user = User.new
-  end
+    def create
+        @user = User.new(params[:user])
+        if @user.save
+            #handle a successful save.
+        else
+            render 'new'
+        end
+    end
 end
