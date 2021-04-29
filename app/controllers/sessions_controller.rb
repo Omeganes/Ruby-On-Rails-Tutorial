6ef0318cd50e21feb81f5a7133db4f45b15ec4f3
Raymond Youssef <rimonomega@gmail.com>
2021-04-29 21:12:10 +0200

    Implement forgetting users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,25 @@
 class SessionsController < ApplicationController
 
-  def new
-  end
-
-  def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
-      reset_session
-      log_in user
-      redirect_to user
-    else
-      # Create an error message.
-      flash.now[:danger] = "Invalid email/password combination" #Not quite right
-      render 'new'
+    def new
     end
-  end
 
-  def destroy
-    log_out
-    redirect_to root_path
-  end
+    def create
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user&.authenticate(params[:session][:password])
+            reset_session
+            remember user
+            log_in user
+            redirect_to user
+        else
+            # Create an error message.
+            flash.now[:danger] = "Invalid email/password combination" #Not quite right
+            render 'new'
+        end
+    end
+
+    def destroy
+        log_out
+        redirect_to root_path
+    end
 
 end
