c67fc88052c6a6eb218109b706525e1cf2aee3bf
Raymond Youssef <rimonomega@gmail.com>
2021-05-02 17:53:56 +0200

    Reconfig for heroku production

--- config/puma.rb
+++ config/puma.rb
@@ -30,9 +30,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 # "only run in production environment"
-if Rails.env.production?
-    workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-end
+# workers ENV.fetch("WEB_CONCURRENCY") { 2 } if Rails.env.production?
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
