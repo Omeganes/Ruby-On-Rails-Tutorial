cc2cc1ea38dbaeb39349aa74aac053bc309cb96d
Raymond Youssef <rimonomega@gmail.com>
2021-05-08 02:04:04 +0200

    Add user microposts

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -10,6 +10,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
         log_in_as(@user)
         get root_path
         assert_select 'div.pagination', count: 1
+        assert_select 'input[type=file]'
         # Invalid submission
         assert_no_difference 'Micropost.count' do
             post microposts_path, params: { micropost: { content: "" } }
@@ -18,9 +19,13 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
         assert_select 'a[href=?]', '/?page=2'
         # Valid submission
         content = "This micropost really ties the room together"
+        image = fixture_file_upload('test/fixtures/kitten.jpg', 'image/jpeg')
         assert_difference 'Micropost.count', 1 do
-            post microposts_path, params: { micropost: { content: content } }
+            post microposts_path, params: { micropost: {
+                content: content, image: image
+            }}
         end
+        assert assigns(:micropost).image.attached?
         assert_redirected_to root_url
         follow_redirect!
         assert_match content, response.body
@@ -34,6 +39,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
         get user_path(users(:yasmin))
         assert_select 'a', { text: 'delete', count: 0 }
     end
+
 
     test "micropost sidebar count" do
         log_in_as(@user)
