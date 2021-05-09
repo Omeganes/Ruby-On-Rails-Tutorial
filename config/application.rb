0091ad9d3db67851416673632fd6ca274bf84090
Raymond Youssef <rimonomega@gmail.com>
2021-05-09 22:10:48 +0200

    Implement follow and unfollow using AJAX

--- config/application.rb
+++ config/application.rb
@@ -7,16 +7,18 @@ require "rails/all"
 Bundler.require(*Rails.groups)
 
 module SampleApp
-  class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    class Application < Rails::Application
+        # Initialize configuration defaults for originally generated Rails version.
+        config.load_defaults 6.1
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
-  end
+        # Configuration for the application, engines, and railties goes here.
+        #
+        # These settings can be overridden in specific environments using the files
+        # in config/environments, which are processed later.
+        #
+        # config.time_zone = "Central Time (US & Canada)"
+        # config.eager_load_paths << Rails.root.join("extras")
+        # Include the authenticity token in remote forms.
+        config.action_view.embed_authenticity_token_in_remote_forms = true
+    end
 end
